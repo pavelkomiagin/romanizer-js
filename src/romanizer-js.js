f65ff9b808326d7d10240ff8c3acb2f1afe09c12
Pavel Komiagin <pavel.komiagin@gmail.com>
2015-10-28 18:51:32 +0300

    Add converter from roman to arabic

--- src/romanizer-js.js
+++ src/romanizer-js.js
@@ -5,16 +5,17 @@ var Romanizer = function(a, b) {
   var arabicNumbers = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
 
   var romanize = function(arabic) {
+    var romanized = '';
+    var arabicNumber;
+    
     arabic = parseInt(arabic);
 
     if (isNaN(arabic) || arabic < 1 || arabic > 3999) {
       throw new Error('Expected input number between 0 and 4000');
     }
 
-    var romanized = '';
-
     romanChars.forEach(function (value, index) {
-      var arabicNumber = arabicNumbers[index];
+      arabicNumber = arabicNumbers[index];
       while (arabic >= arabicNumber) {
         romanized += value;
         arabic -= arabicNumber;
@@ -23,8 +24,42 @@ var Romanizer = function(a, b) {
 
     return romanized;
   };
+  
+  function deromanize(roman) {
+    var arabicResult = 0;
+    var max = roman.length
+    var counter = 0;
+    var currentIndex;
+    var nextIndex;
+    var currentArabicNumber;
+    var nextArabicNumber;
+    
+    roman = roman.toUpperCase();
+    
+    if (roman.replace(/[IVXDLCM]/gi, '').length) {
+      throw new Error('Not valid symbols in roman numeral');
+    }
+    
+    while (counter < max) {
+        currentIndex = romanChars.indexOf(roman.charAt(counter));
+        nextIndex = romanChars.indexOf(roman.charAt(counter + 1));
+        currentArabicNumber = arabicNumbers[currentIndex];
+        nextArabicNumber = arabicNumbers[currentIndex]
+        
+        if (counter + 1 < max && nextArabicNumber > currentArabicNumber) {
+            arabicResult += nextArabicNumber - currentArabicNumber;
+            counter += 2;
+        } else {
+            arabicResult += currentArabicNumber;
+            counter += 1;
+        }
+    }
+    
+    return arabicResult;
+  };
 
   return {
+    deromanize: deromanize,
     romanize: romanize
   };
 };
